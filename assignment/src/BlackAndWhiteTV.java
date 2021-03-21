c1ade764f5b2c08c93e48b5f9789efd33fcc84f1
841355 <841355@cognizant.com>
2021-03-21 20:50:46 +0530

    Week1 Assignment

---
+++ assignment/src/BlackAndWhiteTV.java
@@ -0,0 +1,5 @@
+public class BlackAndWhiteTV {
+    public void selection(){
+        System.out.println("You have selected Black and White TV");
+    }
+}
---
+++ assignment/src/ColourTV.java
@@ -0,0 +1,5 @@
+public class ColourTV extends BlackAndWhiteTV {
+    public void selction() {
+        System.out.println("You have selected colour TV");
+    }
+}
