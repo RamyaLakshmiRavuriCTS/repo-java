662b97b2db0d23c483df0aef7276c4a2280e3459
root <root@gmail.com>
2021-03-25 13:31:14 +0530

    week 1 assignment

---
+++ assignment/src/com/assignment/dynamicBinding/Black.java
@@ -0,0 +1,11 @@
+package com.assignment.dynamicBinding;
+
+public class Black extends OverRiding {
+
+    //overriden method to display black
+    @Override
+    public void display()
+    {
+        System.out.println("You have selected Black");
+    }
+}
