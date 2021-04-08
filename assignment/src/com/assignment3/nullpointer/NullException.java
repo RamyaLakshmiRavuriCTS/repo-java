3460d9f595c7b3c954d5957d08169555b50482dc
eltjavatraining <eltjavatraining>
2021-04-08 15:52:51 +0530

    Week3 assignment

--- assignment/src/com/assignment3/nullpointer/NullException.java
+++ assignment/src/com/assignment3/nullpointer/NullException.java
@@ -8,20 +8,19 @@ public class NullException {
         try{
             int number1=30;
             int number2=0;
-
             int result =number1/number2;
+            
+            String name=null;
+            int l=name.length();
         }
         catch(ArithmeticException e){
             System.out.print("End of main");
             System.out.println("");
         }
-        try{
-            String name=null;
-            name.length();
-        }
         catch(NullPointerException e)
         {
             System.out.print("End of main 2");
         }
+
     }
 }
--- assignment/src/com/assignment3/thread3/DeadLock.java
+++ assignment/src/com/assignment3/thread3/DeadLock.java
@@ -1,17 +1,15 @@
 package com.assignment3.thread3;
 
-public class DeadLock {
-    /*
-    This is the main method
-     */
-    public static void main(String[] args) throws InterruptedException {
-        Object object1 = new Object();
-        Object object2 = new Object();
-        Thread thread1 = new Thread(new SynchroniseThread(object1, object2), "thread1");
-        Thread thread2 = new Thread(new SynchroniseThread(object1, object2), "thread2");
-        thread1.start();
-        Thread.sleep(2000);
-        thread2.start();
-        Thread.sleep(2000);
-    }
+public class DeadLock{
+        public static void main(String[] args) throws InterruptedException
+        {
+            Object object1 = new Object();
+            Object object2 = new Object();
+            Thread thread1 = new Thread(new SynchroniseThread(object1,object2), "thread1");
+            Thread thread2 = new Thread(new SynchroniseThread(object1,object2), "thread2");
+            thread1.start();
+            Thread.sleep(2000);
+            thread2.start();
+            Thread.sleep(2000);
+        }
 }
