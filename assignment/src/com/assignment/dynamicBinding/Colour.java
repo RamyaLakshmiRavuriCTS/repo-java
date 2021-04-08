91e704d7fdad994b1a363104e84297442f72f17a
Sweetha1908 <81679787+Sweetha1908@users.noreply.github.com>
2021-04-08 12:41:42 +0530

    Update Colour.java

--- assignment/src/com/assignment/dynamicBinding/Colour.java
+++ assignment/src/com/assignment/dynamicBinding/Colour.java
@@ -5,7 +5,6 @@ public class Colour extends OverRiding {
     //overriden method to display white
     @Override
     public void display(){
-
         System.out.println("You have selected Colour");
     }
 }
