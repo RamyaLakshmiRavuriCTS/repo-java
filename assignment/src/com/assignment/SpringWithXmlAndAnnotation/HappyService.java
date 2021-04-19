f75a88ee81e6781385b2f5b0d4134ee1686b7398
Sweetha1908 <81679787+Sweetha1908@users.noreply.github.com>
2021-04-19 14:23:48 +0530

    Please review (#37)
    * Please review
    
    * Update Service.java
    
    * Update AnnotationTest.java
    
    * Update HappyService.java
    
    Co-authored-by: root <root@gmail.com>

---
+++ assignment/src/com/assignment/SpringWithXmlAndAnnotation/HappyService.java
@@ -0,0 +1,11 @@
+package com.assignment.SpringWithXmlAndAnnotation;
+
+public class HappyService implements Service {
+
+	//implementation methods
+	@Override
+	public String getDailyService() {	
+		return "Happily serviced the volleyball team!!";
+	}
+
+}
---
+++ assignment/src/com/assignment/SpringWithXmlAndAnnotation/Service.java
@@ -0,0 +1,6 @@
+package com.assignment.SpringWithXmlAndAnnotation;
+
+//interface class
+public interface Service {
+	public String getDailyService();
+}
